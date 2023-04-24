5e30c7f9a89ab4c1e4281ccacd821b239ac3f8bf
Josiah Parkhill <parkhilljosiah@gmail.com>
2023-04-24 14:03:06 -0400

    first commit initializing background and handling theme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  function handleTheme() {
+    let body = document.getElementById("body");
+
+    if (body.className === "dark") {
+      body.className = "light";
+    } else {
+      body.className = "dark";
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handleTheme}>theme</button>
     </div>
   );
 }
